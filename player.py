854baf9baad7fedd99231ad9fb8b68bbd0d61fa3
Britt <brittbannister1127@icloud.com>
2021-01-22 20:40:20 -0500

    defined player class and used inheritence for human player and computer player.

---
+++ player.py
@@ -0,0 +1,42 @@
+import math
+import random
+
+
+class Player:
+    def __init__(self, letter):
+        #letter is an x or o 
+        self.letter = letter
+
+    # want players to get their next move given in a game.
+    def get_move(self, game):
+        pass
+
+
+class RandomComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        square = random.choice(game.available_moves())
+        return square
+
+
+class HumanPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        valid_square = False
+        val = None
+        while not valid_square:
+            square = input(self.letter + '\'s. Input move (0-9):')
+            try:
+                val = int(square)
+                if val not in game.available_moves():
+                    raise ValueError
+                valid_square = True
+            except ValueError:
+                print('Invalid square! Please try again.')
+
+        return val
+
